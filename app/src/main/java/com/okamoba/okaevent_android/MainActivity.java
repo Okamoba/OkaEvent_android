a1b8d685af60b05fcb8f16f15491dd223f36c8e1
TomokoKondo <to.2a721@gmail.com>
2017-12-10 23:31:19 +0900

    add Firebase Authentication

--- app/src/main/java/com/okamoba/okaevent_android/MainActivity.java
+++ app/src/main/java/com/okamoba/okaevent_android/MainActivity.java
@@ -1,13 +1,72 @@
 package com.okamoba.okaevent_android;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    // ログ用のタグ
+    private String TAG = "MainActivity";
+
+    // Firebase認証
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //-------------------------------------------------------------------------------
+        /*
+        Firebase認証
+         */
+        // Firebaseの認証インスタンスの取得
+        mAuth = FirebaseAuth.getInstance();
+        // Firebaseの認証リスナーを作成
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.e(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.e(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+        // サインインできてないときは、サインインする
+        if (mAuth.getCurrentUser() == null) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+        //-------------------------------------------------------------------------------
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // Firebase認証リスナー追加
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // Firebase認証リスナー解除
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
