2fdc80d9a464effad8d950e4c0b7418851554e56
keiichilo <keiichiloooo@gmail.com>
2018-11-08 15:53:57 +0900

    design

--- app/src/main/java/com/okamoba/okaevent_android/EventModel.java
+++ app/src/main/java/com/okamoba/okaevent_android/EventModel.java
@@ -1,15 +1,21 @@
 package com.okamoba.okaevent_android;
 
 import android.support.annotation.NonNull;
+import android.util.JsonWriter;
 import android.util.Log;
 
 import com.google.firebase.firestore.DocumentSnapshot;
 
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
+import java.io.Writer;
+import java.security.spec.ECField;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -17,106 +23,83 @@ import java.util.Map;
  */
 
 public class EventModel {
+
     @NonNull
-    private String name = "";
-    @NonNull
-    private String text = "";
-    @NonNull
-    private String address = "";
-    @NonNull
-    private Date start_datetime = new Date();
-    @NonNull
-    private Date end_datetime = new Date();
-    @NonNull
-    private String url = "";
-    @NonNull
-    private String author = "";
-    @NonNull
-    private String document_id = "";
+    private Map<String, Object> event_map = new HashMap<String, Object>();
 
     @NonNull
     public String getName() {
-        return name;
+        return checkMapValueNull("name");
     }
 
     public void setName(@NonNull String name) {
-        this.name = name;
+        event_map.put("name", name);
     }
 
     @NonNull
     public String getText() {
-        return text;
+        return checkMapValueNull("text");
     }
 
     public void setText(@NonNull String text) {
-        this.text = text;
+        event_map.put("text", text);
     }
 
     @NonNull
     public String getAddress() {
-        return address;
+        return checkMapValueNull("address");
     }
 
     public void setAddress(@NonNull String address) {
-        this.address = address;
+        event_map.put("address", address);
     }
 
     @NonNull
     public Date getStart_datetime() {
-        return start_datetime;
+        return checkMapDateNull("start_datetime");
     }
 
     public void setStart_datetime(@NonNull Date start_datetime) {
-        this.start_datetime = start_datetime;
+        event_map.put("start_datetime", start_datetime);
     }
 
     @NonNull
     public Date getEnd_datetime() {
-        return end_datetime;
+        return checkMapDateNull("end_datetime");
     }
 
     public void setEnd_datetime(@NonNull Date end_datetime) {
-        this.end_datetime = end_datetime;
+        event_map.put("end_datetime", end_datetime);
     }
 
     @NonNull
     public String getUrl() {
-        return url;
+        return checkMapValueNull("url");
     }
 
     public void setUrl(@NonNull String url) {
-        this.url = url;
+        event_map.put("url", url);
     }
 
     @NonNull
     public String getAuthor() {
-        return author;
+        return checkMapValueNull("author");
     }
 
     public void setAuthor(@NonNull String uid) {
-        this.author = uid;
+        event_map.put("author", uid);
     }
 
     @NonNull
     public String getDocument_id() {
-        return document_id;
+        return checkMapValueNull("document_id");
     }
 
     public void setDocument_id(@NonNull String document_id) {
-        this.document_id = document_id;
+        event_map.put("document_id", document_id);
     }
 
     public Map<String, Object> getEvent() {
-        Map<String, Object> event_map = new HashMap<>();
-
-        event_map.put("name", name);
-        event_map.put("text", text);
-        event_map.put("address", address);
-        event_map.put("start_datetime", start_datetime);
-        event_map.put("end_datetime", end_datetime);
-        event_map.put("url", url);
-        event_map.put("author", author);
-
         return event_map;
     }
 
@@ -125,37 +108,68 @@ public class EventModel {
 
     EventModel(DocumentSnapshot documentSnapshot) {
         Map<String, Object> map = documentSnapshot.getData();
-
-        name = map.get("name").toString();
-        text = map.get("text").toString();
-        address = map.get("address").toString();
-        start_datetime = new Date(map.get("start_datetime").toString());
-        end_datetime = new Date(map.get("end_datetime").toString());
-        url = map.get("url").toString();
-        author = map.get("author").toString();
-        document_id = documentSnapshot.getId();
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            event_map.put(entry.getKey(), entry.getValue());
+        }
+        event_map.put("document_id", documentSnapshot.getId());
     }
 
     EventModel(@NonNull byte[] value) {
         BufferedReader br = new BufferedReader(new StringReader(new String(value)));
 
         try {
-            name = br.readLine();
-            text = br.readLine();
-            address = br.readLine();
-            start_datetime = new Date(br.readLine());
-            end_datetime = new Date(br.readLine());
-            url = br.readLine();
-            author = br.readLine();
-            document_id = br.readLine();
-        } catch (IOException e) {
+            JSONObject jsonObject = new JSONObject(new String(value));
+            Iterator<String> iterator = jsonObject.keys();
+            String key;
+            while (iterator.hasNext()) {
+                key = iterator.next();
+                event_map.put(key, jsonObject.get(key));
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public byte[] getByte() {
-        String return_text = name + "\n" + text + "\n" + address + "\n" + start_datetime.toString() + "\n" + end_datetime.toString() + "\n" + url + "\n" + author + "\n" + document_id;
+        String event_data_string = "";
+        try {
+            JSONObject jsonObject = new JSONObject();
+            for (Map.Entry<String, Object> entry : event_map.entrySet()) {
+                jsonObject.put(entry.getKey(), entry.getValue());
+            }
+            event_data_string = jsonObject.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        return return_text.getBytes();
+        return event_data_string.getBytes();
+    }
+
+    private String checkMapValueNull(String key) {
+        Object value = event_map.get(key);
+        if (value != null) {
+            return value.toString();
+        } else {
+            return "";
+        }
+    }
+
+    private Date checkMapDateNull(String key) {
+        Object value = event_map.get(key);
+        if (value != null) {
+            return getDateFromString(value.toString());
+        } else {
+            return new Date();
+        }
+    }
+
+    private Date getDateFromString(String date_string) {
+        Date date;
+        try {
+            date = new Date(date_string);
+        } catch (Exception e) {
+            date = new Date();
+        }
+        return date;
     }
 }
